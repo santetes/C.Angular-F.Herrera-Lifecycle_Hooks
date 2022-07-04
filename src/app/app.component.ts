24e38749c589b73949b169907041d42597081e5a
Santos <sanmartgon@gmail.com>
2022-07-04 18:21:27 +0200

    fin de la sección

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'lifecycle';
+  mostrar: boolean = true;
+  nombre: string = '';
+
+  alternar() {
+    this.mostrar = !this.mostrar;
+  }
+  reciboNombre(nombre: string) {
+    this.nombre = nombre;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Pagina1Component } from './pages/pagina1/pagina1.component';
+import { MuestraNombreComponent } from './component/muestra-nombre/muestra-nombre.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, Pagina1Component, MuestraNombreComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
